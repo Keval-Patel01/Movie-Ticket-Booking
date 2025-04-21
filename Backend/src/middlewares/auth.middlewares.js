c29573035b93c58365328d3ee1d50c9ede53080c
Keval-Patel01 <kevalpatel011104@gmail.com>
2025-04-21 17:13:22 +0530

    Update auth.middlewares.js

--- Backend/src/middlewares/auth.middlewares.js
+++ Backend/src/middlewares/auth.middlewares.js
@@ -33,56 +33,6 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
 
 export { verifyJWT };
 
-// export const verifyAdminJWT = asyncHandler(async (req, _, next) => {
-//   try {
-//     const token = req.body?.accessToken || req.cookies?.accessToken;
-
-//     if (!token) {
-//       throw new ApiError(401, "Unauthorized: No token provided");
-//     }
-
-//     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-//     // Check if the token belongs to the admin
-//     if (decodeToken.id !== "admin_unique_id") {
-//       throw new ApiError(403, "Forbidden: Admin access required");
-//     }
-
-//     req.admin = {
-//       _id: "admin_unique_id",
-//       email: process.env.ADMIN_EMAIL,
-//     };
-
-//     next();
-//   } catch (error) {
-//     throw new ApiError(401, "Invalid or expired admin access token");
-//   }
-// });
-// export const verifyAdminJWT = asyncHandler(async (req, _, next) => {
-//   try {
-//     const token = req.body?.accessToken || req.cookies?.accessToken;
-
-//     if (!token) {
-//       throw new ApiError(401, "Unauthorized: No token provided");
-//     }
-
-//     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-//     // Check if the token belongs to the admin
-//     if (decodeToken.id !== "admin_unique_id") {
-//       throw new ApiError(403, "Forbidden: Admin access required");
-//     }
-
-//     req.admin = {
-//       _id: "admin_unique_id",
-//       email: process.env.ADMIN_EMAIL,
-//     };
-
-//     next();
-//   } catch (error) {
-//     throw new ApiError(401, "Invalid or expired admin access token");
-//   }
-// });
 
 export const verifyAdminJWT = asyncHandler(async (req, _, next) => {
   try {
